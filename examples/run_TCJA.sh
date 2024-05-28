1172147c8d08546858e0661844daae22be23e135
Weifeng Zhong <weifeng@weifengzhong.com>
2024-05-28 01:08:28 -0400

    change file names and rerun some

--- examples/run_TCJA.sh
+++ examples/run_TCJA.sh
@@ -12,24 +12,34 @@
 # 	done
 # done
 
-python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.2 --g_y_annual=0.04
-python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.4 --g_y_annual=0.02
-python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.4 --g_y_annual=0.03
-python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.4 --g_y_annual=0.04
-python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.6 --g_y_annual=0.02
-python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.6 --g_y_annual=0.03
-python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.6 --g_y_annual=0.04
+# python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.2 --g_y_annual=0.04
+# python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.4 --g_y_annual=0.02
+# python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.4 --g_y_annual=0.03
+# python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.4 --g_y_annual=0.04
+# python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.6 --g_y_annual=0.02
+# python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.6 --g_y_annual=0.03
+# python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.6 --g_y_annual=0.04
 
-for frisch in `seq 0.4 0.1 0.5`
-do
-	for zeta_D in `seq 0.2 0.2 0.6`
-	do
-		for g_y_annual in `seq 0.02 0.01 0.04`
-		do
-			# for tG1 in `seq 10 10 30`
-			# do
-				python run_og_usa_ext.py --frisch=$frisch --zeta_D=$zeta_D --g_y_annual=$g_y_annual
-			# done
-		done
-	done
-done
+# for frisch in `seq 0.4 0.1 0.5`
+# do
+# 	for zeta_D in `seq 0.2 0.2 0.6`
+# 	do
+# 		for g_y_annual in `seq 0.02 0.01 0.04`
+# 		do
+# 			# for tG1 in `seq 10 10 30`
+# 			# do
+# 				python run_og_usa_ext.py --frisch=$frisch --zeta_D=$zeta_D --g_y_annual=$g_y_annual
+# 			# done
+# 		done
+# 	done
+# done
+
+python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.2 --g_y_annual=0.02
+python run_og_usa_ext.py --frisch=0.3 --zeta_D=0.6 --g_y_annual=0.02
+python run_og_usa_ext.py --frisch=0.4 --zeta_D=0.2 --g_y_annual=0.02
+python run_og_usa_ext.py --frisch=0.4 --zeta_D=0.6 --g_y_annual=0.02
+python run_og_usa_ext.py --frisch=0.4 --zeta_D=0.6 --g_y_annual=0.04
+python run_og_usa_ext.py --frisch=0.5 --zeta_D=0.2 --g_y_annual=0.02
+python run_og_usa_ext.py --frisch=0.5 --zeta_D=0.4 --g_y_annual=0.04
+python run_og_usa_ext.py --frisch=0.5 --zeta_D=0.6 --g_y_annual=0.02
+python run_og_usa_ext.py --frisch=0.5 --zeta_D=0.6 --g_y_annual=0.04
--- examples/run_og_usa_ext.py
+++ examples/run_og_usa_ext.py
@@ -22,10 +22,10 @@ plt.style.use(style_file_url)
 
 def main(frisch = None, zeta_D = None, g_y_annual = None, tG1 = None):
 
-    example_dir = "TCJA<>frisch<>" + str(frisch) + \
-                  "<>zeta_D<>" + str(zeta_D) + \
-                  "<>g_y_annual<>" + str(g_y_annual) + \
-                  "<>tG1<>" + str(tG1)
+    example_dir = "TCJA##frisch##" + str(frisch) + \
+                  "##zeta_D##" + str(zeta_D) + \
+                  "##g_y_annual##" + str(g_y_annual) + \
+                  "##tG1##" + str(tG1)
 
     # Define parameters to use for multiprocessing
     num_workers = min(multiprocessing.cpu_count(), 5)
